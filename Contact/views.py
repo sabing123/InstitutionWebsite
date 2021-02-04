f6a0c4634002d855637df7df1f9d5906b8ba231e
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-02-04 19:22:41 +0545

    Contact page added

---
+++ Contact/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ContactForm
+from django.contrib import messages
+# Create your views here.
+def contactview(request):
+    name=''
+    email=''
+    phone=''
+    subject=''
+    message=''
+    form= ContactForm(request.POST or None)
+    if form.is_valid():
+        name= form.cleaned_data.get("name")
+        email= form.cleaned_data.get("email")
+        phone= form.cleaned_data.get("phone")
+        subject= form.cleaned_data.get("subject")
+        message=form.cleaned_data.get("message")
+        message= name + " with the email, " + email + ", sent the following message:\n\n" + message;
+        send_mail(subject, message, 'rupesh.thapa2050@gmail.com', [email])
+        context= {'form': form}
+        messages.info(request, 'Your message has been sent.')
+        return render(request, 'contact.html', context)
+    else:
+        context= {'form': form}
+    return render(request, 'contact.html', context)
