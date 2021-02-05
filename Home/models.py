ad19bb371b229f39aaf0ce56c25aa2d3eab589c0
AnilThapa5 <thapaanil307@gmail.com>
2021-02-05 12:06:53 +0545

    blog section added

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='pics')
+    title = models.CharField(max_length=120)
+    date = models.DateField(auto_now=True)
+    postedBy = models.TextField(max_length=30)
+    description = models.TextField(max_length=200)
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def blog(request):
+    blog1 = Blog.objects.all()
+    return render(request, "blog.html", {'blog1': blog1})
